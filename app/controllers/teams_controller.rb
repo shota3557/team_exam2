1f2415e1c2fc54bb029a0dc59925c670d4ba809c
shota kikuchi <shota3557@gmail.com>
2022-11-21 14:46:05 +0900

    リーダ権限移動した時にメール送れるよう設定した

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_team, only: %i[show edit update destroy]
+  before_action :set_team, only: %i[show edit update destroy assign_owner]
 
   def index
     @teams = Team.all
@@ -49,6 +49,13 @@ class TeamsController < ApplicationController
 
   def dashboard
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
+  end
+
+  def assign_owner
+    @team.update(owner_id: params[:owner_id])
+    TeamMailer.team_mail(@team).deliver
+    @user = User.find(@team.owner_id)
+    redirect_to team_path, notice: 'オーナー権限が移動しました'
   end
 
   private
